d692aac8265add3edfc79587147fee5ce2f829c1
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-04 23:24:51 +0530

    add Tasks on CSV file

---
+++ api_service/tasks.py
@@ -0,0 +1,88 @@
+from __future__ import absolute_import, unicode_literals
+import uuid
+import json
+
+import pandas as pd
+from pandas import ExcelWriter
+from celery import shared_task
+
+from api_service import models
+
+
+def get_dataset(dataset_id):
+    dataset = models.DatasetModel.objects.get(id=dataset_id)
+    return dataset
+
+@shared_task
+def process_dataset(dataset_id):
+    save_dataframe(dataset_id)
+
+
+def save_dataframe(dataset_id):
+    dataset = get_dataset(dataset_id)
+    data = pd.read_csv(f'{dataset.file.name}')
+    df = pd.DataFrame(data)
+
+    name = f'{str(uuid.uuid4())}.feather'
+    df.to_feather(name)
+
+    # Save dataframe details
+    dataset.dataframe_file_name = name
+    dataset.dataframe_creation_status = 'CRT'
+    dataset.dataframe_url = f'/media/{name}'
+    dataset.save()
+
+    # Process further
+    export_excel.delay(dataset_id)
+    export_json.delay(dataset_id)
+    export_histogram_pdf.delay(dataset_id)
+
+
+@shared_task
+def export_excel(dataset_id):
+    dataset = get_dataset(dataset_id)
+    data = pd.read_feather(dataset.dataframe_file_name)
+    df = pd.DataFrame(data)
+
+    name = f'{str(uuid.uuid4())}.xlsx'
+
+    with ExcelWriter(name) as writer:
+        df.to_excel(writer)
+
+    # Save excel details
+    dataset.excel_file_name = name
+    dataset.excel_creation_status = 'CRT'
+    dataset.excel_url = f'/media/{name}'
+    dataset.save()
+
+
+@shared_task
+def export_json(dataset_id):
+    dataset = get_dataset(dataset_id)
+    data = pd.read_feather(dataset.dataframe_file_name)
+    df = pd.DataFrame(data)
+
+    json_data = df.describe().to_json()
+
+    # Save json details
+    dataset.stats = json.dumps(json_data)
+    dataset.stats_creation_status = 'CRT'
+    dataset.save()
+
+
+@shared_task
+def export_histogram_pdf(dataset_id):
+    dataset = get_dataset(dataset_id)
+    data = pd.read_feather(dataset.dataframe_file_name)
+    df = pd.DataFrame(data)
+    ax = df.plot.hist()
+    figure = ax.get_figure()
+
+    name = f'{str(uuid.uuid4())}.pdf'
+    figure.savefig(name)
+
+    # Save PDF details
+    dataset.pdf_file_name = name
+    dataset.pdf_creation_status = 'CRT'
+    dataset.pdf_url = f'/media/{name}'
+    dataset.save()
--- api_service/views.py
+++ api_service/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 
 from api_service import serializers
 from api_service import models
+from api_service import tasks
 
 
 @api_view(['GET', 'POST'])
@@ -16,39 +17,40 @@ def datasets_view(request):
 
     data = resp_status = None
 
-    try:
-        serializer = None
-        if request.method == 'GET':
-            # GET request - return all datasets
-            datasets = models.DatasetModel.objects.all()
-            serializer = serializers.DatasetSerializer(datasets, many=True)
-            resp_status = status.HTTP_200_OK
+    # try:
+    serializer = None
+    if request.method == 'GET':
+        # GET request - return all datasets
+        datasets = models.DatasetModel.objects.all()
+        serializer = serializers.DatasetSerializer(datasets, many=True)
+        resp_status = status.HTTP_200_OK
+
+    else:
+        # POST request - create a new dataset object
+        serializer = serializers.DatasetSerializer(data=request.data, many=False)
+        if serializer.is_valid():
+            serializer.save()
+
+            # Store the size of the dataset
+            dataset_id = serializer.data.get('id')
+            dataset = models.DatasetModel.objects.get(id=dataset_id)
+            dataset.size = dataset.file.size
+            dataset.save()
+
+            serializer = serializers.DatasetSerializer(dataset, many=False)
+            resp_status = status.HTTP_201_CREATED
+
+            # Begin processing using Pandas
+            tasks.process_dataset.delay(dataset_id)
 
         else:
-            # POST request - create a new dataset object
-            serializer = serializers.DatasetSerializer(data=request.data, many=False)
-            if serializer.is_valid():
-                serializer.save()
+            raise Exception(f'{serializer.errors}')
 
-                # Store the size of the dataset
-                dataset_id = serializer.data.get('id')
-                dataset = models.DatasetModel.objects.get(id=dataset_id)
-                dataset.size = dataset.file.size
-                dataset.save()
+    data = {'success': True, 'data': serializer.data}
 
-                serializer = serializers.DatasetSerializer(dataset, many=False)
-                resp_status = status.HTTP_201_CREATED
+    # except Exception as e:
+    #     data = {'success': False, 'message': f'Error: {str(e)}'}
+    #     resp_status = status.HTTP_400_BAD_REQUEST
 
-                
-
-            else:
-                raise Exception(f'{serializer.errors}')
-
-        data = {'success': True, 'data': serializer.data}
-
-    except Exception as e:
-        data = {'success': False, 'message': f'Error: {str(e)}'}
-        resp_status = status.HTTP_400_BAD_REQUEST
-
-    finally:
-        return Response(data=data, status=resp_status)
+    # finally:
+    return Response(data=data, status=resp_status)

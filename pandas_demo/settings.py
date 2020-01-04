c6c03acbfb67f3ba4fd08628c5bbaf28ba56b809
Praneeth Karnena <praneeth.codes@gmail.com>
2020-01-05 00:04:51 +0530

    implement media

--- pandas_demo/settings.py
+++ pandas_demo/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
 
 # DRF settings
 REST_FRAMEWORK = {
